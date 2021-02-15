32f70c320a80aaf3183de7a973502ab46788975c
Danil27 <danil27dzr@gmail.com>
2021-02-15 08:23:02 +0300

    axios plugin

---
+++ src/plugins/axios.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+instance.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token')
+  if (typeof token === 'string') {
+    config.headers['Authorization'] = `${token.replaceAll('"', '')}`
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+instance.interceptors.response.use(function (response) {
+  return response
+}, function (error) {
+  return Promise.reject(error)
+})
+
+export default instance
